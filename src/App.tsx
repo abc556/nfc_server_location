84748dc830c67d837a2a3c7ee20e45095699a3f2
Justin Lo <justinlctstudy96@gmail.com>
2022-03-14 16:55:58 +0800

    changed another qrcode library which works with chinese

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import { useQRCode } from 'react-qrcodes';
-import QRCode from "react-qr-code"
 import muselabsPng from './media/muselabs.png'
+import { useQRCode } from 'next-qrcode';
 
 function App() {
   
+  const { Canvas } = useQRCode();
   const [value, setValue] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={muselabsPng} className="navbarImg" />
-        <input className="valueInput" value={value} onChange={e => setValue(e.target.value)} type={"text"} />
-        <QRCode value={value} />
+          <input className="valueInput" value={value} onChange={e => setValue(e.target.value)} type={"text"} />
+        <div className="qrCodeContainer">
+          <Canvas
+            text={value!=""?value:"null"}
+            options={{
+              type: 'image/jpeg',
+              quality: 0.3,
+              level: 'M',
+              margin: 3,
+              scale: 4,
+              width: 200,
+              color: {
+                dark: '#010599FF',
+                light: '#FFBF60FF',
+              },
+            }}
+          />
+        </div>
       </header>
     </div>
   );
